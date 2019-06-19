332d60312c5c2a63fef7ff74d580c6266c57255b
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-19 13:00:01 +0530

    Create 1211110031 : Number Addition.c

---
+++ level_01/session_01/1211110031 : Number Addition.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{	int i, arr[2],sum=0;
+ for(i=0;i<2;i++){
+ cin>>arr[i];
+   
+ }
+ for(i=0;i<2;i++){
+ sum=sum+arr[i];
+   
+ }
+ cout<<sum;
+
+	return 0;
+}
