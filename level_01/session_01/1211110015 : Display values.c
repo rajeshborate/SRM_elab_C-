58a14d4d65fa5d3b779eef90049774f8fa69cf28
Rajesh Borate <rajeshborate08@gmail.com>
2019-06-19 13:01:03 +0530

    Create 1211110015 : Display values.c

---
+++ level_01/session_01/1211110015 : Display values.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{	int arr[5],i;
+ for(i=0;i<5;i++){
+ cin>>arr[i];
+   
+ }
+ for(i=0;i<5;i++){
+ cout<<arr[i]<<"\n";
+   
+ }
+
+	return 0;
+}
